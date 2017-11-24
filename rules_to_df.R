2ac2634bc523172d659f68e128e29de450097b3e
Francesco Imbriglia <imbrigliaf@yoox.net>
2017-11-24 11:08:11 +0100

    Creating .funs to convert rules into df

---
+++ rules_to_df.R
@@ -0,0 +1,18 @@
+### High level function in order to convert "rules"-class object into dataframe 
+### It also converts many-to-one into one-to-one rules format
+
+arules.to.df <- function(rules) {
+  
+  require(dplyr)
+  
+  ### Control on object class
+  if(class(rules) != 'rules'){
+    warning('L\'argomento non è di classe \'rules\'')
+  }
+  
+  ### Eseg
+  else {
+    rules.df <- as.data.frame(inspect(rules))
+    
+  }
+}
